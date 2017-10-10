fafa5be46fd343d751c063ce30acec73789d5502
zhl752 <32107137+zhl752@users.noreply.github.com>
2017-10-10 13:56:14 -0500

    Add files via upload

---
+++ linkedList.java
@@ -0,0 +1,48 @@
+/* Name:Zhiwei LIN
+     
+*/
+
+
+package linkedList;
+import java.util.LinkedList;
+import java.util.ListIterator;
+public class linkedList {
+      
+	
+	
+	
+	
+	
+	
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	LinkedList<String> address = new LinkedList<String>();
+	 address.add("Tulsa");
+	 address.add("Ada");
+	 address.add("Broken Arrow");
+	 address.add("Owasso");
+	 address.add(1,"OKC");
+	 while(address.hasNext()&& aIer.hasNext()) {
+		 address.remove(1);
+	 }
+	 
+	LinkedList<String> data= new LinkedList<String>();
+	 data.add("74104");
+	 data.addFirst("74135");
+	 data.add("foo");
+	 data.add("hello world");
+	 data.add("777");
+	 
+	ListIterator<String> aIer= address.listIterator();
+	ListIterator<String> dIer= data.listIterator();
+	  
+	LinkedList<String> copydata= new LinkedList<String>();
+    while(data!= null) { 
+	   copydata.add(dIer.next());
+   }
+   
+    if
+	  
+	}
+
+}
